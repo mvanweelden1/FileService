eb094a1a0be49f9a3ef4a4f57f79a0610e615ac1
mvanweelden1@my.wctc.edu <>
2013-05-04 19:49:04 -0500

    Writer works with format strategy

--- src/fileservice/FileWriterStrategy.java
+++ src/fileservice/FileWriterStrategy.java
@@ -7,10 +7,10 @@ import java.util.List;
  *
  * @author Mark Van Weelden <mvanweelden1@my.wctc.edu>
  */
-public interface FileWriterStrategy {
+public interface FileWriterStrategy<T> {
     
-    public abstract void writeLineToFile(String data, boolean append)throws IOException;
     
-    public abstract void writeAllToFile(List<String> data, boolean append)throws IOException;
+    
+    public abstract void writeToFile(String filePath, T data, boolean append)throws IOException;
 
 }
--- src/fileservice/TextFileWriter.java
+++ src/fileservice/TextFileWriter.java
@@ -6,82 +6,73 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
  *
  * @author Mark Van Weelden <mvanweelden1@my.wctc.edu>
  */
-public class TextFileWriter implements FileWriterStrategy {
+public class TextFileWriter implements FileWriterStrategy<List<LinkedHashMap<String, String>>> {
 
-    private File file;
+    private FormatStrategy<List<LinkedHashMap<String, String>>, List<String>> formatter;
 
-    public TextFileWriter(String filePath) {
-        setFile(new File(filePath));
+    public TextFileWriter(FormatStrategy<List<LinkedHashMap<String, String>>, List<String>> formatter) {
+        this.formatter = formatter;
     }
 
     @Override
-    public void writeLineToFile(String data, boolean append) throws IOException{
+    public void writeToFile(String filePath, List<LinkedHashMap<String, String>> data, boolean append) throws IOException {
+
+        File file = new File(filePath);
         PrintWriter writer = null;
+
         try {
             writer = new PrintWriter(new BufferedWriter(new FileWriter(file, append)));
-            writer.println(data);
-
-        } catch (IOException ex) {
-            throw ex;
+            String encodedData = formatter.encode(data);
+            writer.print(encodedData);
+        } catch (IOException ioe) {
+            throw ioe;
         } finally {
             try {
                 writer.close();
             } catch (Exception e) {
             }
         }
-    }
-
-    public final File getFile() {
-        return file;
-    }
-
-    public final void setFile(File file) {
-        this.file = file;
-    }
-
-    @Override
-    public void writeAllToFile(List<String> data, boolean append) throws IOException{
-
-        List<String> inputData = new ArrayList<String>(data);
-        PrintWriter writer = null;
-
-        try {
-            writer = new PrintWriter(new BufferedWriter(new FileWriter(file, append)));
-             for (String string : inputData) {
-                writer.println(string);
-                
-            }
-        } catch (IOException ioe) {
-            throw ioe;
-        }finally{
-            try {
-                writer.close();
-            } catch (Exception e) {
-            }
-        }
 
 
 
     }
 
+    
+
+    public FormatStrategy<List<LinkedHashMap<String, String>>, List<String>> getFormatter() {
+        return formatter;
+    }
+
+    public void setFormatter(FormatStrategy<List<LinkedHashMap<String, String>>, List<String>> formatter) {
+        this.formatter = formatter;
+    }
+    
     public static void main(String[] args) throws IOException {
-        TextFileWriter writer1 = new TextFileWriter("src/test.out");
-
-        List<String> data = new ArrayList<String>();
-        data.add("Jane,Smith,555 West Ave.,Waukesha,WI,55555,e@b.com,555-555-5558");
-        data.add("John,Smith,555 West Ave.,Waukesha,WI,55555,f@b.com,555-555-5558");
-        data.add("Jack,Anderson,555 West Ave.,Waukesha,WI,55555,g@b.com,555-555-5558");
-
-//        for (String string : data) {
-//            writer1.writeLineToFile(string, true);
-//        }
-        
-        writer1.writeAllToFile(data, false);
+       LinkedHashMap<String, String> map1 = new LinkedHashMap<>();
+       map1.put("Hours Parked: ", "22.0");
+       map1.put("Fees Collected", "$10.00");
+       
+       LinkedHashMap<String, String> map2 = new LinkedHashMap<>();
+       map2.put("Hours Parked: ", "3.25");
+       map2.put("Fees Collected", "$2.50");
+       
+       List<LinkedHashMap<String, String>> data = new ArrayList<>();
+       
+       data.add(map1);
+       data.add(map2);
+       
+       TextFileWriter writer = new TextFileWriter(new CsvCommaFormat(true));
+       String filePath = File.separatorChar + "temp" + File.separatorChar 
+                + "parkedHours.txt";
+       
+       writer.writeToFile(filePath, data, true);
+       
     }
 }
